47fcd3cc106c2428daa44296c93e30e4b4f33084
phcentenaro7 <phc7blu@gmail.com>
2024-07-01 10:06:35 -0300

    Add files via upload

--- motor.py
+++ motor.py
@@ -2,19 +2,6 @@ import RPi.GPIO as GPIO
 from time import sleep
 import sys
         
-#Speed in steps per second
-def run_steps(dir_pin, step_pin, ena_pin, sensor_pin, rotation, sleep_time, steps):
-    for steps in range(steps):
-        GPIO.output(dir_pin, rotation)
-        if sensor_pin != 0 and GPIO.input(sensor_pin) == 0:
-            GPIO.output(step_pin, GPIO.LOW)
-            return
-        else:
-            GPIO.output(step_pin, GPIO.HIGH)
-        sleep(sleep_time)
-        GPIO.output(step_pin, GPIO.LOW)
-        sleep(sleep_time)
-        
 #Clockwise/Counterclockwise rotation values
 CW = 1 #Away from motor
 CCW = 0 #Closer to motor
@@ -45,9 +32,9 @@ GPIO.setup(STEP_Y, GPIO.OUT)
 GPIO.setup(ENA_Y, GPIO.OUT)
 GPIO.setup(SENSOR_X_BEGIN, GPIO.IN)
 GPIO.setup(SENSOR_Y_BEGIN, GPIO.IN)
-GPIO.setup(RED_BUTTON, GPIO.IN)
-GPIO.setup(YELLOW_BUTTON, GPIO.IN)
-GPIO.setup(GREEN_BUTTON, GPIO.IN)
+GPIO.setup(RED_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(YELLOW_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(GREEN_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(RED_LED, GPIO.OUT)
 GPIO.setup(YELLOW_LED, GPIO.OUT)
 GPIO.setup(GREEN_LED, GPIO.OUT)
@@ -57,31 +44,61 @@ GPIO.output(GREEN_LED, GPIO.HIGH)
 GPIO.output(ENA_X, GPIO.LOW)
 GPIO.output(ENA_Y, GPIO.LOW)
 
-STATE = 1
+EMERGENCY = 0
+START_STATE = 1
+AT_ORIGIN = 2
+ROUTING = 3
+STATE = START_STATE
+        
+#Speed in steps per second
+def run_steps(dir_pin, step_pin, ena_pin, sensor_pin, rotation, sleep_time, steps):
+    for step in range(int(steps)):
+        if GPIO.input(RED_BUTTON) == GPIO.HIGH:
+            GPIO.output(RED_LED, GPIO.HIGH)
+            GPIO.output(YELLOW_LED, GPIO.LOW)
+            GPIO.output(GREEN_LED, GPIO.LOW)
+            sleep(3)
+            STATE = EMERGENCY
+            return
+        GPIO.output(dir_pin, rotation)
+        if sensor_pin != 0 and GPIO.input(sensor_pin) == 0:
+            GPIO.output(step_pin, GPIO.LOW)
+            return
+        else:
+            GPIO.output(step_pin, GPIO.HIGH)
+        sleep(sleep_time)
+        GPIO.output(step_pin, GPIO.LOW)
+        sleep(sleep_time)
 
 try:
     while True:
         GPIO.output(GREEN_LED, GPIO.HIGH)
-        if GPIO.input(GREEN_BUTTON) == 1 and STATE == 1:
+        if GPIO.input(GREEN_BUTTON) == 1 and STATE == START_STATE:
             GPIO.output(GREEN_LED, GPIO.LOW)
             GPIO.output(YELLOW_LED, GPIO.HIGH)
             run_steps(DIR_Y, STEP_Y, ENA_Y, SENSOR_Y_BEGIN, CCW, SLEEP_TIME, sys.maxsize)
             run_steps(DIR_X, STEP_X, ENA_X, SENSOR_X_BEGIN, CCW, SLEEP_TIME, sys.maxsize)
-            STATE = 2
+            STATE = AT_ORIGIN
             GPIO.output(GREEN_LED, GPIO.HIGH)
-        elif GPIO.input(YELLOW_BUTTON) == 1 and STATE == 2:
+        elif GPIO.input(YELLOW_BUTTON) == GPIO.HIGH and STATE == AT_ORIGIN:
+            STATE = ROUTING
             GPIO.output(GREEN_LED, GPIO.LOW)
-            run_steps(DIR_Y, STEP_Y, ENA_Y, 0, CW, SLEEP_TIME, 5*CM)
-            run_steps(DIR_X, STEP_X, ENA_X, 0, CW, SLEEP_TIME, 9*CM)
-            sleep(3)
-            run_steps(DIR_Y, STEP_Y, ENA_Y, 0, CCW, SLEEP_TIME, 2*CM)
-            sleep(3)
-            run_steps(DIR_X, STEP_X, ENA_X, 0, CW, SLEEP_TIME, 2*CM)
-            sleep(3)
-            run_steps(DIR_Y, STEP_Y, ENA_Y, 0, CW, SLEEP_TIME, 2*CM)
-            sleep(3)
+            run_steps(DIR_Y, STEP_Y, ENA_Y, 0, CW, SLEEP_TIME, 3.7*CM)
+            run_steps(DIR_X, STEP_X, ENA_X, 0, CW, SLEEP_TIME, 6.35*CM)
+            sleep(5)
+            run_steps(DIR_Y, STEP_Y, ENA_Y, 0, CCW, SLEEP_TIME, 1.6*CM)
+            sleep(5)
+            run_steps(DIR_X, STEP_X, ENA_X, 0, CW, SLEEP_TIME, 1.5*CM)
+            sleep(5)
+            run_steps(DIR_Y, STEP_Y, ENA_Y, 0, CW, SLEEP_TIME, 1.6*CM)
+            sleep(5)
             GPIO.output(YELLOW_LED, GPIO.LOW)
-            STATE = 1
+            STATE = START_STATE
+        elif STATE == EMERGENCY:
+            if GPIO.input(RED_BUTTON) == GPIO.HIGH:
+                GPIO.output(RED_LED, GPIO.LOW)
+                GPIO.output(GREEN_LED, GPIO.HIGH)
+                STATE = START_STATE
 
 except KeyboardInterrupt:
     GPIO.output(RED_LED, GPIO.LOW)
